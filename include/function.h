cbe3b0386f3be94c51fb68a75efd9e1765dd8a34
Deedat <fdeedat9@gmail.com>
2022-12-08 13:28:07 +0700

    integration v1

--- include/function.h
+++ include/function.h
@@ -5,8 +5,9 @@
 
 #define echoPin 26 // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin 25 //attach pin D3 Arduino to pin Trig of HC-SR0
-#define LED 3
-#define flowSens 27
+#define LED 2
+#define flowSens 32
+#define relay 25
 
 void usSig();
 void lcdInit();
