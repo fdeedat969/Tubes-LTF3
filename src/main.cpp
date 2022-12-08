aab6b543596dbe999681596be3ed9500c1b6b00e
Deedat <fdeedat9@gmail.com>
2022-12-08 13:29:01 +0700

    integration v1

--- src/main.cpp
+++ src/main.cpp
@@ -21,20 +21,23 @@ byte pin_column[COLUMN_NUM] = {14, 27, 26};
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
 
 // defines variables
-int duration; // variable for the duration of sound wave travel
-float height = 0; // variable for the distance measurement
-unsigned long previousTime = 0;
-unsigned long interval1 = 500;
-unsigned long interval2 = 1000;
+long currentMillis = 0;
+long previousMillis = 0;
+int interval = 1000;
+int interval1 = 250;
+boolean ledState = LOW;
+float calibrationFactor = 4.5;
 volatile byte pulseCount;
-byte pulse1sec = 0;
+byte pulse1Sec = 0;
 float flowRate;
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres = 0;
 
 char dataKey[5];
 int dataKeyInt;
 int i = 0;
 
-// this two function should be moved to function.cpp but idk how to someone pls fix
+// this three function should be moved to function.cpp but idk how to someone pls fix
 void clearData(){
   while (i != 0){
     dataKey[i--] = 0;
@@ -58,49 +61,47 @@ void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
   pinMode(LED, OUTPUT);
+  pinMode(relay, OUTPUT);
   pinMode(flowSens,INPUT_PULLUP);
-  Serial.begin(9600); // Serial Communication is starting with 9600 of baudrate speed
+
+  pulseCount = 0;
+  flowRate = 0.0;
+  flowMilliLitres = 0;
+  previousMillis = 0;
+  Serial.begin(9600);
+
   attachInterrupt(digitalPinToInterrupt(flowSens), pulseCounter, FALLING);
 }
 
 void loop() {
   // multitasking with millis
-  // Ultrasonic Reading 
-  unsigned long currentTime = millis();
-  if (currentTime - previousTime >= interval1) { 
-    previousTime = currentTime;
-    usSig();
-    duration = pulseIn(echoPin,HIGH);
-
-    height = 19.44 - (0.0178*duration - 0.7605);
-    lcdPrint(height);
-    //Serial.print("Distance: ");
-    //Serial.println(height);
-   }
-
-  // Flowsensor Reading
-  if (currentTime - previousTime >= interval2) { 
-    previousTime = currentTime;
-    pulse1sec = pulseCount;
-    pulseCount = 0;
-    flowRate = ((float)pulse1sec);
-
-    Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));
-  }
-
+  currentMillis = millis();
+  // Keypad Reading
   char keypressed = keypad.getKey();
   if (keypressed){
     if (keypressed == '*'){
       backSpace();
     } else if(keypressed == '#'){
       dataKeyInt = atoi(dataKey);
-      if (height >= dataKeyInt){
-        digitalWrite(LED, HIGH);
-      } else {
-        digitalWrite(LED, LOW);
-      }
+      Serial.print("Input: ");
+      Serial.println(dataKeyInt);
       clearData();
+      while(totalMilliLitres < dataKeyInt){
+        digitalWrite(LED, HIGH);
+        delay(2000);
+        if (currentMillis - previousMillis > interval) {
+          
+          pulse1Sec = pulseCount;
+          pulseCount = 0;
+
+          flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
+          previousMillis = millis();
+
+          flowMilliLitres = (flowRate / 60) * 1000;
+        }
+      }
+      digitalWrite(LED, LOW);
+      totalMilliLitres = 0;
     }
     else{
       dataKey[i] = keypressed;
@@ -109,5 +110,35 @@ void loop() {
       Serial.println(dataKey);
       i++; 
     }
-  }
+  }   
+  // // Ultrasonic Reading 
+  // currentMillis = millis();
+  // if (currentMillis- previousMillis > interval1) { 
+  //   previousMillis= currentMillis;
+  //   usSig();
+  //   int duration = pulseIn(echoPin,HIGH);
+
+  //   float volume= -6.8804*(duration) + 12943;
+  //   lcdPrint(volume);
+  //   Serial.print("Volume: ");
+  //   Serial.print(volume);
+  //   Serial.println(" ml");
+  //  }
+
+  // // Flowsensor Reading
+  // if (currentMillis - previousMillis > interval) {
+    
+  //   pulse1Sec = pulseCount;
+  //   pulseCount = 0;
+
+  //   flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
+  //   previousMillis = millis();
+
+  //   flowMilliLitres = (flowRate / 60) * 1000;
+
+
+    // Serial.print("Flow rate: ");
+    // Serial.print(flowMilliLitres);  // Print the integer part of the variable
+    // Serial.print("mL/s");
+    // Serial.print("\t");       // Print tab space
 }
